8fddc8627f21f36b6d9e1298bb90e944bb19095b
Fermat's Fish <FermatsFish@users.noreply.github.com>
2020-04-18 14:43:45 +1200

    Add GameController and TreeController + Some initial tree creation code

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public static GameController instance;
+
+    public GameObject treePrefab;
+
+    List<TreeController> trees = new List<TreeController>();
+
+    const float TREE_UPDATE_PERIOD = 5f;
+
+    float treeTimer = 0f;
+
+    void Start()
+    {
+        // Establish instance.
+        if (instance != null)
+        {
+            Debug.LogError("There is already a GameController");
+        }
+        else
+        {
+            instance = this;
+        }
+
+    }
+
+    void CreateTree(Vector2 position)
+    {
+        // Instantiate the new tree.
+        var newTreeGO = Instantiate(treePrefab, new Vector3(position.x, position.y, TreeController.TREE_Z), Quaternion.identity);
+        var newTreeController = newTreeGO.GetComponent<TreeController>();
+
+        // Add to the list of trees.
+        trees.Add(newTreeController);
+    }
+
+    void Update()
+    {
+        treeTimer += Time.deltaTime;
+
+        while (treeTimer > TREE_UPDATE_PERIOD)
+        {
+            foreach (var tree in trees)
+            {
+                tree.Grow();
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/TreeController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreeController : MonoBehaviour
+{
+    public const int TREE_Z = 0;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+
+    public void Grow()
+    {
+        //TODO Impliment
+    }
+}
